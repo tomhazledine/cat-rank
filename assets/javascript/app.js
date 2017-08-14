ad1764728711b4c0d2bd45f4318c3df63e506da7
Thomas Hazledine <tom.hazledine@headforwards.com>
2017-08-14 11:08:32 +0100

    basic up-down and votes functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,12 +8,96 @@ MyApp.addRegions({
 
 
 
-AngryCat = Backbone.Model.extend({});
+AngryCat = Backbone.Model.extend({
+
+    defaults: {
+        votes: 0
+    },
+
+    addVote: function(){
+        this.set('votes', this.get('votes') + 1);
+    },
+
+    rankUp: function() {
+        // this.addVote();
+        this.set({rank: this.get('rank') - 1});
+    },
+
+    rankDown: function() {
+        // this.addVote();
+        this.set({rank: this.get('rank') + 1});
+    }
+});
 
 
 
 AngryCats = Backbone.Collection.extend({
-    model: AngryCat
+    model: AngryCat,
+
+    initialize: function(cats){
+        var rank = 1;
+        _.each(cats, function(cat) {
+            console.log(cat);
+            cat.set('rank', rank);
+            ++rank;
+        });
+
+        var self = this;
+
+        MyApp.on('rank:up', function(cat){
+            cat.addVote();
+            if (cat.get('rank') === 1) {
+                // can't increase rank of top-ranked cat
+                return true;
+            }
+            self.rankUp(cat);
+            self.sort();
+            self.trigger('reset');
+        });
+
+        MyApp.on('rank:down', function(cat){
+            cat.addVote();
+            if (cat.get('rank') === self.size()) {
+                // can't decrease rank of lowest ranked cat
+                return true;
+            }
+            self.rankDown(cat);
+            self.sort();
+            self.trigger('reset');
+        });
+
+        this.on('add', function(cat){
+            if( ! cat.get('rank') ) {
+                var error =  Error('Cat must have a rank defined before being added to the collection');
+                error.name = 'NoRankError';
+                throw error;
+            }
+        });
+    },
+
+    comparator: function(cat) {
+        return cat.get('rank');
+    },
+ 
+    rankUp: function(cat) {
+        // find the cat we're going to swap ranks with
+        var rankToSwap = cat.get('rank') - 1;
+        var otherCat = this.at(rankToSwap - 1);
+
+        // swap ranks
+        cat.rankUp();
+        otherCat.rankDown();
+    },
+
+    rankDown: function(cat) {
+        // find the cat we're going to swap ranks with
+        var rankToSwap = cat.get('rank') + 1;
+        var otherCat = this.at(rankToSwap - 1);
+
+        // swap ranks
+        cat.rankDown();
+        otherCat.rankUp();
+    }
 });
 
 
@@ -21,7 +105,25 @@ AngryCats = Backbone.Collection.extend({
 AngryCatView = Backbone.Marionette.ItemView.extend({
     template: "#angry_cat-template",
     tagName: 'tr',
-    className: 'angry_cat'
+    className: 'angry_cat',
+
+    events: {
+        'click .rank_up img': 'rankUp',
+        'click .rank_down img': 'rankDown'
+    },
+
+    initialize: function(){
+        this.bindTo(this.model, 'change:votes', this.render);
+        // this.listenTo(this.model, "change:votes", this.render);
+    },
+
+    rankUp: function(){
+        MyApp.trigger('rank:up', this.model);
+    },
+
+    rankDown: function(){
+        MyApp.trigger('rank:down', this.model);
+    }
 });
 
 
@@ -56,10 +158,16 @@ MyApp.addInitializer(function(options){
 
 $(document).ready(function(){
     var cats = new AngryCats([
-        { name: 'Wet Cat' },
-        { name: 'Bitey Cat' },
-        { name: 'Surprised Cat' }
+        new AngryCat({ name: 'Wet Cat', image_path: 'assets/images/cat2.jpg' }),
+        new AngryCat({ name: 'Bitey Cat', image_path: 'assets/images/cat1.jpg' }),
+        new AngryCat({ name: 'Surprised Cat', image_path: 'assets/images/cat3.jpg' })
     ]);
 
     MyApp.start({cats: cats});
+
+    cats.add(new AngryCat({
+        name: 'Cranky Cat',
+        image_path: 'assets/images/cat4.jpg',
+        rank: cats.size() + 1
+    }));
 });
